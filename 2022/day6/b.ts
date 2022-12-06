3862cb0faa6e80de270ddc47109e55ad58cfddcd
Zakaria Abdelaziz <zikk.abdelaziz@gmail.com>
2022-12-06 12:14:03 +0100

    day 6 part 2

--- 2022/day6/b.ts
+++ 2022/day6/b.ts
@@ -1,1 +1,18 @@
-const input = await Deno.readTextFile('./input.test');
+const input = await Deno.readTextFile('./input.prod');
+
+const result = input.split('').reduce<string[]>((stack, char) => {
+  if (stack.length === 14) {
+    return stack;
+  }
+
+  if (stack.includes(char)) {
+    const charIndex = stack.findIndex((c) => c === char);
+    stack = [...stack.slice(charIndex + 1), char];
+    return stack;
+  }
+
+  stack.push(char);
+  return stack;
+}, [])
+
+console.log(input.indexOf(result.join('')) + result.join('').length);
