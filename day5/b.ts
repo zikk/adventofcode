8a5bb22c616313f765357fd354ae7de7f9ed45c2
Zakaria Abdelaziz <zikk.abdelaziz@gmail.com>
2022-12-06 01:37:16 +0100

    day 5 part 2

---
+++ day5/b.ts
@@ -0,0 +1,37 @@
+const input = await Deno.readTextFile('./input.prod');
+
+const numberOfStacks = (input.split('\n')[0].length + 1) / 4;
+let stacks = new Array(numberOfStacks).fill([]);
+let shouldSkip = false;
+let indicesLineIndex = 0;
+
+input.split('\n').forEach((line, lineIndex) => {
+  if (shouldSkip) return;
+
+  for (let i = 0; i < numberOfStacks * 4 - 1; i += 4) {
+    const crateIndex = i / 4;
+    const crate = line.slice(i, i + 4)[1];
+
+    if (crate !== ' ' && !Number.isInteger(Number.parseInt(crate, 10))) {
+      stacks[crateIndex] = [...stacks[crateIndex], crate];
+    }
+
+    if (Number.isInteger(Number.parseInt(crate, 10))) {
+      shouldSkip = true;
+    }
+  }
+
+  indicesLineIndex = lineIndex;
+});
+
+stacks = stacks.map((stack) => stack.reverse());
+const commands = input.split('\n').slice(indicesLineIndex + 2);
+
+commands.forEach((command) => {
+  const digits = command.match(/\d+/g)?.map((digit) => Number.parseInt(digit, 10)) ?? [];
+  const crates = stacks[digits[1] - 1].splice(stacks[digits[1] - 1].length - digits[0], digits[0]);
+  stacks[digits[2] - 1].push(...crates);
+})
+
+const result = stacks.map((stack) => stack.pop()).join('')
+console.log(result);
